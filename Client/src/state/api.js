d99e33a33cec952a6d2ce353dcd7740d137e24e3
schawla@starbucks.com <schawla@starbucks.com>
2023-08-22 14:02:40 +0530

    changes for server

--- Client/src/state/api.js
+++ Client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:1337" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
   reducerPath: "main",
   tagTypes: [],
   endpoints: (build) => ({
